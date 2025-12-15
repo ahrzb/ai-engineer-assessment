0baa0e191750ba96b726d79fdb58012811aefbbe
AmirHossein Roozbahani <ahrzb5@gmail.com>
2025-12-15 21:32:47 +0100

    Integrate mapbox

--- backend/src/backend/mapbox_client.py
+++ backend/src/backend/mapbox_client.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 import os
 from typing import Optional
+from backend.similarity import address_similarity
+import httpx
 
 from dotenv import load_dotenv
 
@@ -10,6 +12,7 @@ load_dotenv()
 class MapboxClient:
     def __init__(self, token: str | None = None) -> None:
         self.token = token or os.getenv("MAPBOX_ACCESS_TOKEN")
+        self.client = httpx.Client()
 
         if not self.token:
             raise Exception("MAPBOX_ACCESS_TOKEN must be set")
@@ -18,5 +21,12 @@ class MapboxClient:
         url = "https://api.mapbox.com/search/geocode/v6/forward"
 
         # TODO: implement function to find the best match and return it here
+        request = self.client.get(url, params={"access_token": self.token, "q": query})
 
-        return f"Match for {query}"
+        addresses = [response["properties"]["full_address"] for response in request.json()["features"]]
+        if not addresses:
+            return None
+
+        addresses.sort(key=lambda x: address_similarity(query, x))
+
+        return addresses[-1]
